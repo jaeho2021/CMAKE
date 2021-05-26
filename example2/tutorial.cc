fe1f5a4f8e51ac3b4e64088047c84bb549135710
jaeho83.lim <jaeho83.lim@lge.com>
2021-05-26 18:27:26 +0900

    [JH] add comment for example2

--- example2/tutorial.cc
+++ example2/tutorial.cc
@@ -4,7 +4,7 @@
 
 int main (int argc, char*argv[] )
 {
-	// report version
+	// TutorialConfig.h 파일에 선언된 매트로 출력
 	std::cout << " Version : " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
 
 	return 1;
