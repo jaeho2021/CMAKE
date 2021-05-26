64b0b5ed805a85750a9ddb9cd1f696fce7197654
jaeho83.lim <jaeho83.lim@lge.com>
2021-05-26 10:39:07 +0900

    [JH] cmake usage#2

---
+++ example2/tutorial.cc
@@ -0,0 +1,11 @@
+
+#include "TutorialConfig.h"
+#include <iostream>
+
+int main (int argc, char*argv[] )
+{
+	// report version
+	std::cout << " Version : " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+
+	return 1;
+}
